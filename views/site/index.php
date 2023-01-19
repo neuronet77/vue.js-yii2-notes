4ec8793eff4263cf2ff292f8edff63426e2cd2b7
neuronet77 <a.hinderberger@yahoo.ca>
2023-01-19 14:01:53 -0700

    Update index.php

--- views/site/index.php
+++ views/site/index.php
@@ -7,11 +7,11 @@ $this->title = 'My Yii Application';
 <div class="site-index">
 
     <div class="jumbotron">
-        <h1 style="font-size: 48px;">Welcome to <a href="https://notes.thecodeholic.com">notes.thecodeholic.com</a></h1>
+        <h1 style="font-size: 48px;">Welcome to <a href="https://notes.thecodeholic.com">notes.gertex.us</a></h1>
 
         <p class="lead">The is the API domain.
           <br>Click the
-          <a href="https://notes.thecodeholic.com">following link</a> if you want to see demo.
+          <a href="https://notes.gertex.us">following link</a> if you want to see the app.
         </p>
     </div>
 </div>
