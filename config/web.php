86c64db5cbaeea022f7bca1667b6d0d190cb1dfe
neuronet77 <a.hinderberger@yahoo.ca>
2023-01-19 14:06:21 -0700

    Update web.php

--- config/web.php
+++ config/web.php
@@ -5,7 +5,7 @@ $db = require __DIR__ . '/db.php';
 
 $config = [
     'id' => 'basic',
-    'name' => 'TheCodeholic\'s notes',
+    'name' => 'GerTeX Notes',
     'basePath' => dirname(__DIR__),
     'bootstrap' => ['log'],
     'aliases' => [
